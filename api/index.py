5a8c7bd7251d5cd486b2a6568831dcf94a71db06
James Smith <james@loopj.com>
2023-05-27 20:38:59 -0700

    Slight cleanup

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 import hashlib
 import json
 import os
-from flask import Flask, request, Response
+from flask import Flask, request, Response, abort
 
 app = Flask(__name__)
 
@@ -12,31 +12,34 @@ with open(file_path, "r") as f:
     shows = [line.strip() for line in f.readlines()]
 
 
-@app.route("/api")
-def get_matching_shows():
-    jsonp = False
-    if "callback" in request.args:
-        jsonp = True
-
-    matching_shows = []
-    if "q" in request.args:
-        for show in shows:
-            if request.args["q"] in show.lower():
-                matching_shows.append(
-                    {
-                        "id": hashlib.md5(show.encode()).hexdigest(),
-                        "name": show,
-                    }
-                )
-
-    response_str = json.dumps(matching_shows)
-    if jsonp:
-        response_str = f"{request.args['callback']}({response_str})"
+# Create a JSON response, optionally wrapping it in a JSONP callback
+def jsonify(data, jsonp_callback=False):
+    response_str = json.dumps(data)
+    if jsonp_callback:
+        response_str = f"{jsonp_callback}({response_str})"
 
     resp = Response(response_str)
-    if jsonp:
-        resp.headers['Content-Type'] = 'application/javascript'
+    if jsonp_callback:
+        resp.headers["Content-Type"] = "application/javascript"
     else:
-        resp.headers['Content-Type'] = 'application/json'
+        resp.headers["Content-Type"] = "application/json"
 
     return resp
+
+
+@app.route("/api")
+def get_matching_shows():
+    if "q" not in request.args:
+        abort(400)
+
+    matching_shows = []
+    for show in shows:
+        if request.args["q"] in show.lower():
+            matching_shows.append(
+                {
+                    "id": hashlib.md5(show.encode()).hexdigest(),
+                    "name": show,
+                }
+            )
+
+    return jsonify(matching_shows, request.args.get("callback", False))
